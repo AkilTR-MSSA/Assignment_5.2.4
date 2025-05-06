5d4e539886711a65aeb51d935cc00949c2d2bf86
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-06 15:27:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Input a string: ");
+string input = Console.ReadLine();
+Console.WriteLine(IsPalindrome_Recursive(input, 0 , input.Length - 1));
+
+bool IsPalindrome_Recursive(string s, int left, int right) {
+    if (left >= right)
+    {
+        return true;
+    }
+    if (s[left] != s[right])
+    {
+        return false;
+    }
+    return IsPalindrome_Recursive(s, left + 1, right - 1);
+}
